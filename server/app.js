af884fbd8a26affe8bb60401ea43f0f2dd469cc6
NandithaSridharan <nandithas.it2023@citchennai.net>
2025-12-26 23:51:33 +0530

    user model and authentication(login and register)

--- server/app.js
+++ server/app.js
@@ -1,15 +1,9 @@
 const express = require("express");
-const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-// Middlewares
-app.use(cors());
 app.use(express.json());
-
-// Health check
-app.get("/", (req, res) => {
-  res.send("Book Haven API is running");
-});
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
